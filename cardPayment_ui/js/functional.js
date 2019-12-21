3a04d6f9e2f57e27972eac330dce54be3c6d6218
Ariel Calisaya <calisayaariel62@gmail.com>
2019-12-21 01:15:19 -0300

    css finished

--- cardPayment_ui/js/functional.js
+++ cardPayment_ui/js/functional.js
@@ -1,1 +1,25 @@
+const card = document.querySelector('#card'),
+        btnOpenform = document.querySelector('#btn-open-form'),
+        form = document.querySelector('#card-form'),
+        cardNumber = document.querySelector('#card .numero'),
+        cardName = document.querySelector('#card .name'),
+        logo = document.querySelector('#logo'),
+        firm = document.querySelector('#card .firm p'),
+        monthExpiration = document.querySelector('#card .month')
 
+const InitFront = () => {
+    if (card.classList.contains('active')){
+        card.classList.remove('active')
+    }
+}
+
+// rotation card func
+card.addEventListener('click', () => {
+    card.classList.toggle('active');
+})
+
+// rotation button & open form func
+btnOpenform.addEventListener('click', () => {
+    btnOpenform.classList.toggle('active')
+    form.classList.toggle('active')
+})
