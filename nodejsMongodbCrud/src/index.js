cb9aeec25f3bec13f904fccf098f2767802edd74
Ariel Calisaya <calisayaariel62@gmail.com>
2020-01-11 18:24:25 -0300

    First Connection with mongodb and mongoose

---
+++ nodejsMongodbCrud/src/index.js
@@ -0,0 +1,40 @@
+const path = require('path')
+const express = require('express');
+const morgan = require('morgan')
+const mongoose = require('mongoose');
+
+const app = express();
+
+// importing Routes
+const indexRoute = require('./routes/index')
+
+// first connection db
+mongoose.connect('mongodb://localhost/mongo-crud', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true
+})
+    .then(db => console.log('DB conected'))
+    .catch(err => console.log(err))
+
+
+
+
+// settings
+app.set('port', process.env.PORT || 3000);
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+
+
+// routes
+app.use('/', indexRoute);
+
+// middleware
+app.use(morgan('dev'));
+app.use(express.urlencoded({extended:false}));
+
+// start server
+app.listen(app.get('port'), () => {
+    console.log(`Running on port: ${app.get('port')}`)
+})
