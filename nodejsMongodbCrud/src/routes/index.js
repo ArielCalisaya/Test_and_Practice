fe660abc838c391c571c715d155360117a75b3d1
Ariel Calisaya <calisayaariel62@gmail.com>
2020-01-14 20:02:46 -0300

    added route to submit form

--- nodejsMongodbCrud/src/routes/index.js
+++ nodejsMongodbCrud/src/routes/index.js
@@ -1,13 +1,17 @@
 const express = require('express');
 const router = express.Router();
 
+const Todo = require('../models/todos');
+
 router.get('/', (req, res) => {
     res.render('to-do')
 });
 
 router.post('/add', (req, res) => {
     console.log(req.body);
-    res.send('Recived')
+    res.send('Recivido')
 })
 
 module.exports = router;
+
+// something is goind wrong req.body == undefined
